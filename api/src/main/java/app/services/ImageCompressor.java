a7f20b32241e0aa6397e2009343029b3d006eb4a
Pavel Evleev <mr.malefiic@gmail.com>
2018-02-11 23:15:46 +0300

    The ability to upload and download pictures from the dropbox was implemented

--- api/src/main/java/app/services/ImageCompressor.java
+++ api/src/main/java/app/services/ImageCompressor.java
@@ -1,5 +1,11 @@
 package app.services;
 
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.FileMetadata;
+import com.dropbox.core.v2.files.UploadErrorException;
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.IIOImage;
@@ -22,18 +28,23 @@ public class ImageCompressor implements Runnable {
 
     private String compressedFile;
 
-    public ImageCompressor(MultipartFile image, String compressedFile) {
+    private DbxClientV2 client;
+
+
+    public ImageCompressor(MultipartFile image, String compressedFile, DbxClientV2 client) {
         this.image = image;
         this.compressedFile = compressedFile;
+        this.client = client;
     }
+
 
     public void compressAndSaveImage(MultipartFile image, String compressedFile) throws IOException {
 
         try (InputStream inputStreamImage = image.getInputStream()) {
-            BufferedImage bufferedImage = ImageIO.read(inputStreamImage);
-            File compressImageFile = new File("D:\\testImage\\" + compressedFile + ".jpg");
 
-            try (OutputStream outputStream = new FileOutputStream(compressImageFile)) {
+            BufferedImage bufferedImage = ImageIO.read(inputStreamImage);
+
+            try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
 
                 Iterator<ImageWriter> imageWriters = ImageIO.getImageWritersByFormatName("jpg");
                 if (!imageWriters.hasNext())
@@ -52,11 +63,19 @@ public class ImageCompressor implements Runnable {
                 } catch (IOException e) {
                     throw e;
                 }
+                byte[] byteImage = outputStream.toByteArray();
+
+                ByteInputStream byteInputStream = new ByteInputStream(byteImage, byteImage.length);
+                FileMetadata metadata = client.files().uploadBuilder("/" + compressedFile + ".jpg")
+                    .uploadAndFinish(byteInputStream);
+
             } catch (IOException e) {
                 throw e;
+            } catch (UploadErrorException e) {
+                e.printStackTrace();
+            } catch (DbxException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
--- api/src/main/java/app/services/ImageService.java
+++ api/src/main/java/app/services/ImageService.java
@@ -1,15 +1,17 @@
 package app.services;
 
+import com.dropbox.core.DbxDownloader;
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.FileMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * Created by Pavel on 24.01.2018.
@@ -20,16 +22,28 @@ public class ImageService {
     @Autowired
     private ThreadPoolTaskExecutor taskExecutor;
 
+
+    private static final String ACCESS_TOKEN = "Nj7jh4RntCAAAAAAAAAAChQOewtSMM4lWqY82zUjEFZPPej9Jo6LCK6IZDtWEHeS";
+
+    private DbxClientV2 client;
+
+    public ImageService() {
+        DbxRequestConfig config = new DbxRequestConfig("dropbox/beworm_image");
+        this.client = new DbxClientV2(config, ACCESS_TOKEN);
+    }
+
     public void compressAndSaveImage(MultipartFile image, String compressedFile) {
-        ImageCompressor compressor = new ImageCompressor(image, compressedFile);
+        ImageCompressor compressor = new ImageCompressor(image, compressedFile, client);
         taskExecutor.execute(compressor);
     }
 
     public byte[] getImage(String fileName) {
         byte[] byteFile = null;
-        File file = new File("D:\\testImage\\" + fileName);
-        try (InputStream stream = new FileInputStream(file)) {
-            byteFile = StreamUtils.copyToByteArray(stream);
+        try {
+            DbxDownloader<FileMetadata> download = client.files().download("/" + fileName);
+            byteFile = StreamUtils.copyToByteArray(download.getInputStream());
+        } catch (DbxException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
